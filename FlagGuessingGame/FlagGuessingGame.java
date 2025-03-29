38b2949a4066197068c64e6f3eae7a06f2dff5f9
Troy Ceralde <117468396+TroyCeralde@users.noreply.github.com>
2025-03-29 16:08:02 +0800

    Update FlagGuessingGame.java

--- FlagGuessingGame/FlagGuessingGame.java
+++ FlagGuessingGame/FlagGuessingGame.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
@@ -21,8 +20,8 @@ public class FlagGuessingGame extends JFrame {
     private String currentFlag;
     private int level;
     private final int maxLevel = 3;
-    private int flagIndex; // To keep track of which flag to show next
-    private final HashSet<String> guessedFlags = new HashSet<>(); // Track guessed flags
+    private int flagIndex;
+    private final HashSet<String> guessedFlags = new HashSet<>();
 
     public FlagGuessingGame() {
         setTitle("Flag Guessing Game");
@@ -33,8 +32,8 @@ public class FlagGuessingGame extends JFrame {
         flagLabel = new JLabel();
         guessField = new JTextField(15);
         submitButton = new JButton("Submit Guess");
-        timerLabel = new JLabel("Time Left: 30");
-        attemptLabel = new JLabel("Attempts Left: 5");
+        timerLabel = new JLabel(); // Timer label without default text
+        attemptLabel = new JLabel(); // Attempt label without default text
 
         add(flagLabel);
         add(guessField);
@@ -44,7 +43,7 @@ public class FlagGuessingGame extends JFrame {
 
         submitButton.addActionListener(e -> checkGuess());
 
-        setLevel(1); // Start at level 1
+        setLevel(1);
         setVisible(true);
     }
 
@@ -55,67 +54,63 @@ public class FlagGuessingGame extends JFrame {
         }
 
         this.level = newLevel;
-        guessedFlags.clear(); // Clear guessed flags for new level
-        flagIndex = 0; // Reset flag index for new level
+        guessedFlags.clear();
+        flagIndex = 0;
         loadFlags();
 
-        // Set time limit or attempts based on level
         switch (level) {
             case 1:
                 timeLeft = 30;
                 timerLabel.setText("Time Left: 30");
-                startTimer(); // Start the timer for level 1
+                attemptLabel.setText(""); // Hide attempt counter
+                startTimer();
                 break;
             case 2:
                 attemptsLeft = 5;
-                attemptLabel.setText("Attempts Left: 5");
+                attemptLabel.setText("Attempts Left: " + attemptsLeft);
+                timerLabel.setText(""); // Hide timer
                 break;
             case 3:
                 attemptsLeft = 3;
-                attemptLabel.setText("Attempts Left: 3");
-                break;
-            default:
+                attemptLabel.setText("Attempts Left: " + attemptsLeft);
+                timerLabel.setText(""); // Hide timer
                 break;
         }
-
         showNextFlag();
     }
 
     private void loadFlags() {
         flags = switch (level) {
-            case 1 ->
-                new String[]{
-                    "FlagImages" + File.separator + "Philippines.png",
-                    "FlagImages" + File.separator + "South Korea.png",
-                    "FlagImages" + File.separator + "India.png",
-                    "FlagImages" + File.separator + "China.png",
-                    "FlagImages" + File.separator + "USA.png",};
-
-            case 2 ->
-                new String[]{
-                    "FlagImages" + File.separator + "Vietnam.png",
-                    "FlagImages" + File.separator + "Spain.png",
-                    "FlagImages" + File.separator + "Thailand.png",
-                    "FlagImages" + File.separator + "Brazil.png",
-                    "FlagImages" + File.separator + "Turkey.png",
-                    "FlagImages" + File.separator + "Venezuela.png",
-                    "FlagImages" + File.separator + "Portugal.png",
-                    "FlagImages" + File.separator + "United Kingdom.png",};
-                    
-            case 3 ->
-                new String[]{
-                    "FlagImages" + File.separator + "Uganda.png",
-                    "FlagImages" + File.separator + "Somalia.png",
-                    "FlagImages" + File.separator + "Slovenia.png",
-                    "FlagImages" + File.separator + "Sri Lanka.png",
-                    "FlagImages" + File.separator + "South Sudan",
-                    "FlagImages" + File.separator + "Uzbekistan.png",
-                    "FlagImages" + File.separator + "Serbia.png",
-                    "FlagImages" + File.separator + "Tanzania",
-                    "FlagImages" + File.separator + "Syria.png",
-                    "FlagImages" + File.separator + "Russia.png",};
-            default ->
-                new String[]{};
+            case 1 -> new String[]{
+                "FlagImages" + File.separator + "Philippines.png",
+                "FlagImages" + File.separator + "South Korea.png",
+                "FlagImages" + File.separator + "India.png",
+                "FlagImages" + File.separator + "China.png",
+                "FlagImages" + File.separator + "USA.png",
+            };
+            case 2 -> new String[]{
+                "FlagImages" + File.separator + "Vietnam.png",
+                "FlagImages" + File.separator + "Spain.png",
+                "FlagImages" + File.separator + "Thailand.png",
+                "FlagImages" + File.separator + "Brazil.png",
+                "FlagImages" + File.separator + "Turkey.png",
+                "FlagImages" + File.separator + "Venezuela.png",
+                "FlagImages" + File.separator + "Portugal.png",
+                "FlagImages" + File.separator + "United Kingdom.png",
+            };
+            case 3 -> new String[]{
+                "FlagImages" + File.separator + "Uganda.png",
+                "FlagImages" + File.separator + "Somalia.png",
+                "FlagImages" + File.separator + "Slovenia.png",
+                "FlagImages" + File.separator + "Sri Lanka.png",
+                "FlagImages" + File.separator + "South Sudan.png",
+                "FlagImages" + File.separator + "Uzbekistan.png",
+                "FlagImages" + File.separator + "Serbia.png",
+                "FlagImages" + File.separator + "Tanzania.png",
+                "FlagImages" + File.separator + "Syria.png",
+                "FlagImages" + File.separator + "Russia.png",
+            };
+            default -> new String[]{};
         };
     }
 
@@ -129,7 +124,7 @@ public class FlagGuessingGame extends JFrame {
             if (timeLeft <= 0) {
                 timer.stop();
                 JOptionPane.showMessageDialog(null, "Time's up! The flag was: " + currentFlag);
-                setLevel(level); // Restart the same level when time is up
+                setLevel(level);
             }
         });
         timer.start();
@@ -153,7 +148,7 @@ public class FlagGuessingGame extends JFrame {
         } else {
             if (level < maxLevel) {
                 JOptionPane.showMessageDialog(this, "All flags guessed! Moving to Level " + (level + 1));
-                setLevel(level + 1); // Move to the next level if all flags guessed
+                setLevel(level + 1);
             } else {
                 JOptionPane.showMessageDialog(this, "You have completed the game!");
             }
@@ -163,19 +158,17 @@ public class FlagGuessingGame extends JFrame {
     private void checkGuess() {
         String guess = guessField.getText().trim();
         if (guess.equalsIgnoreCase(currentFlag)) {
-            guessedFlags.add(currentFlag); // Track correct guess
+            guessedFlags.add(currentFlag);
             JOptionPane.showMessageDialog(null, "Correct! You have guessed " + guessedFlags.size() + " flags.");
-            showNextFlag(); // Show another flag for the same level
+            showNextFlag();
         } else {
             if (level == 1) {
-                // Time-based level (level 1), no attempt counting here
                 JOptionPane.showMessageDialog(null, "Incorrect! Try again.");
             } else {
-                // Levels 2 and 3 use attempts
                 attemptsLeft--;
                 if (attemptsLeft <= 0) {
                     JOptionPane.showMessageDialog(null, "Out of attempts! The flag was: " + currentFlag);
-                    setLevel(level); // Restart same level if attempts run out
+                    setLevel(level);
                 } else {
                     JOptionPane.showMessageDialog(null, "Incorrect! Try again. " + attemptsLeft + " attempts left.");
                     attemptLabel.setText("Attempts Left: " + attemptsLeft);
